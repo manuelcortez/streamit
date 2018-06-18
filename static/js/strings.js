c73d024304e70b8b721a93d8cf4e148427552fe6
Manuel Cortez <manuel@manuelcortez.net>
2018-06-17 21:56:46 -0500

    Added static assets

---
+++ static/js/strings.js
@@ -0,0 +1,10 @@
+var GLOBAL_STRINGS = {
+    "PLAY": "Reproducir",
+    "PAUSE": "Pausa",
+    "TOGGLE_FULL_SCREEN": "Conmutar pantalla completa",
+    "MUTE": "Silenciar",
+    "RESTART": "Reiniciar",
+    "CAPTIONS": "Subtítulos",
+    "REWIND": "Retroceder",
+    "FORWARD": "Avanzar"
+}
