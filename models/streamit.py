c5470b4a7e3e17651e56383d8039eb30c82293b3
Manuel Cortez <manuel@manuelcortez.net>
2019-02-11 23:22:55 +0000

    commented models a little, made movies info page translatable

--- models/streamit.py
+++ models/streamit.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+# A table representation for a TV show. It shows information regarding general data, but not season and episodes are added right here.
+# Hopefully in the future Streamit would be able to update certain information regarding the TV show (for example, number of episodes and seasons)
+# Without needing to create a new database entry.
 db.define_table("tv_show",
 	Field("show_title", "string", required=True),
 	Field("release_date", "date"),
@@ -13,10 +16,16 @@ db.define_table("tv_show",
 	Field("rating", "double"),
 	Field("rating_count", "integer"),
 	Field("overview", "text"),
+	# Genre will be used for searches in the future, so we'll make sure we save it.
 	Field("genre", "string"),
+	# This will contain an image name. Images are saved in static/imgs and here is just a plain reference to it.
 	Field("image", "string"),
+	# Useful to diferentiate content by language.
 	Field("language", "reference languages"))
 
+# this table holds information about a season for a TV show.
+# tv_show is a table reference to an item present in the tv_show table.
+# Here we still save just general info, no episodes nor media files are stored here.
 db.define_table("tv_season",
 	Field("show", "reference tv_show", required=True),
 	Field("name", "string", required=True),
@@ -26,11 +35,14 @@ db.define_table("tv_season",
 	Field("overview", "text"),
 	Field("image", "string"))
 
+# Media files storage.
+# type must be tv or movie, because owner is the ID of the data associated to such file.
 db.define_table("media",
 	Field("type", "string", required=True, requires=IS_IN_SET(("tv", "movie"))),
 	Field("owner", "integer"),
 	Field("file", "string", required=True))
 
+# This table holds information about a movie.
 db.define_table("movie",
 	Field("title", "string", required=True),
 	Field("release_date", "date"),
@@ -44,8 +56,8 @@ db.define_table("movie",
 	Field("overview", "text"),
 	Field("genre", "string"),
 	Field("image", "string"),
-	Field("file", "reference media"),
-	Field("language", "string", requires=IS_IN_DB(db, db.languages.code)))
+	Field("file", "reference media", readable=False, writeable=False),
+	Field("language", "reference languages", required=True))
 
 db.define_table("tv_episode",
 	Field("show", "reference tv_show", required=True),
@@ -58,7 +70,7 @@ db.define_table("tv_episode",
 	Field("rating_count", "integer"),
 	Field("overview", "text"),
 	Field("image", "string"),
-	Field("file", "reference media"))
+	Field("file", "reference media", readable=False, writeable=False))
 
 db.define_table("saved_file",
 	Field("file", "reference media"),
